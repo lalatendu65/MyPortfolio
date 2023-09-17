c05afae689020716c86784fc3075db137a3e0c86
lalatendu <lalatendupany.2000@gmail.com>
2023-09-18 01:38:05 +0530

    path changed

--- src/constants/routes.ts
+++ src/constants/routes.ts
@@ -1,9 +1,9 @@
 export const routes={
-    home:"https://lalatendu65.github.io/MyPortfolio/",
-    projects:"https://lalatendu65.github.io/MyPortfolio/project",
-    about:"https://lalatendu65.github.io/MyPortfolio//about",
-    experience:"https://lalatendu65.github.io/MyPortfolio/experience",
-    contactMe:"https://lalatendu65.github.io/MyPortfolio/contact_me",
-    resume:"/resume",
-    notFound:"*"
+    home:"/MyPortfolio",
+    projects:"/MyPortfolio/project",
+    about:"/MyPortfolio/about",
+    experience:"/MyPortfolio/experience",
+    contactMe:"/MyPortfolio/contact_me",
+    resume:"/MyPortfolio/resume",
+    notFound:"/MyPortfolio/*"
 }
